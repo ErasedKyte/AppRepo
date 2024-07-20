e8bc94c95be882528cf67e96c4fcf1ebdca36691
ErasedKyte <kyouku9900gmail.com>
2024-07-20 17:57:06 +0300

    Revert "nigger"
    This reverts commit 868813f97e58849351cf65f186aad7da257f0e5d.

--- src/app/pages/Acquisition/page.tsx
+++ src/app/pages/Acquisition/page.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 import FileUpload from '../../Components/FileUpload';
 import FileList from '../../Components/FileList';
-import {db} from '../../db'
-import { redirect } from 'next/navigation'
 
-export default function AcquisitionPage() {
-  async function CreateAcq(formData: FormData){
-    'use server';
-    const Docs = formData.get('Docs') as string;
-    const snippet = await db.documents.create({
-      data:{
-        Docs
-      },
-    });
-    redirect('/');
-  }
-  
+const AcquisitionPage: React.FC = () => {
   return (
     <div>
       <h1>Acquisition Page</h1>
-      <form action = {CreateAcq}>
+      <form>
         <input 
         name = "Docs" 
         id = "Docs"
@@ -30,3 +17,4 @@ export default function AcquisitionPage() {
   );
 };
 
+export default AcquisitionPage;
